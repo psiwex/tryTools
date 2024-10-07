0d3520ccac818736817113a1a2086c5ae3895d63
psiwex <130997158+psiwex@users.noreply.github.com>
2024-10-07 15:21:46 -0400

    Add files via upload

--- tryLoaderProto.m
+++ tryLoaderProto.m
@@ -1,5 +1,27 @@
 %T = readtable('myfile.csv');
 %T = readtable('myfile.csv','NumHeaderLines',3);
+
+raw_bdf_loadpath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl';
+% path to save raw datasets as .set/.fdt eeglab format
+raw_dataset_savepath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl';
+% path to save .set/.fdt datasets at intermediate processing steps: optional but strongly recommended
+%   example 1: datasets saved after bad channel rejection can be used to determine which channels were removed and interpolated
+%   example 2: datasets saved after artifact rejection can be used to determine which and how many epochs/trials/conditions were rejected 
+%       Note: This is required to use SummaryScript.m to save details on bad channels/artifact rejection results
+ongoing_dataset_savepath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl';
+% path to save fully processed .set/.fdt files before ERP averaging
+processed_dataset_savepath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl';
+% path to save erpsets in erplab .erp format
+erpset_savepath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl';
+% save raw and processed eventlist path: 
+raw_eventlist_savepath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl';
+processed_eventlist_savepath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl';
+% binlister path: see https://github.com/lucklab/erplab/blob/master/pop_functions/pop_binlister.m
+binlister_loadpath = 'C:\Users\John\Documents\MATLAB\tryEeg\tryEtl\CueBinlister.txt';
+
+
+
+
 mainWindow=30;
 esuf1='a_rest.bdf';
 esuf2='b_rest.bdf';
@@ -66,6 +88,8 @@ hrvstdValues1=[];
 homeDir='C:\Users\John\Documents\MATLAB\tryEeg\CUE_REST\';
 channelLocationFile = 'C:\Users\John\Documents\MATLAB\eeglab2021.1\plugins\dipfit\standard_BESA\standard-10-5-cap385.elp';
 
+
+
 oldSR=1024;
 
 
@@ -77,6 +101,16 @@ subNames=table2cell(T(:,1));
 ki=1;
 subNum='TRY001';
 mki=1;
+
+mkh=1;
+lppNeuCells0={};
+lppAlcCells0={};
+lppFodCells0={};
+lppFixCells0={};
+lppCue={};
+
+
+
 for ki=1:length(subNames)
 subNum=subNames{ki};
 
@@ -101,219 +135,60 @@ try
 timeOutputs1 = getTryTimestampsMeasures(fName1);
 timeOutputs2 = getTryTimestampsMeasures(fName2);
 
+
+
 %% preproc
 %rest
 EEG = pop_biosig(efName1);
 [EEG,params] = tryPreproc(EEG);
 [ekgPeaks, params] = eeg_beats(EEG, params);
+[EEG,ERP,erns] = cuePreproc(efName1,raw_dataset_savepath,ongoing_dataset_savepath,erpset_savepath,binlister_loadpath,channelLocationFile);
 [bpm1, hr1, meanRR1, stdRR1, alphaPower1, alphaStd1,hrv1,hrvStd1] = calcBpm(EEG,ekgPeaks,eegChan);
 
-EEG = pop_biosig(efName2);
-%timeOutputs1 = tryEpochIndex(EEG,timeOutputs1);
-[EEG,params] = tryPreproc(EEG);
-[ekgPeaks, params] = eeg_beats(EEG, params);
-[bpm2, hr2, meanRR2, stdRR2, alphaPower2, alphaStd2,hrv2,hrvStd2] = calcBpm(EEG,ekgPeaks,eegChan);
-
-hr=[meanRR1 meanRR2];
+hr=[meanRR1];
 hrValues(ki,:)=hr;
 
-bpm=[stdRR1 stdRR2];
+bpm=[stdRR1];
 bpmValues(ki,:)=bpm;
 
-ap=[alphaPower1 alphaPower2];
+ap=[alphaPower1];
 apValues(ki,:)=ap;
 
-apstd=[alphaStd1 alphaStd2];
+apstd=[alphaStd1];
 apstdValues(ki,:)=apstd;
 
+hrvValues(ki,:)=hrv1;
 
-%CUE
-EEG = pop_biosig(ffName1);
-timeOutputs1 = tryEpochIndex(EEG,timeOutputs1);
-[EEG,params] = tryPreproc(EEG);
-EEG = tryEpocher(EEG,timeOutputs1);
-[ekgPeaks, params] = eeg_beats(EEG, params);
-eegChan = chanFinder(EEG,plotChan);
-EEG = lppParameters(EEG,timeOutputs1,eegChan);
-[bpm11, hr11, meanRR11, stdRR11, alphaPower11, alphaStd11,hrv11,hrvStd11] = calcBpm(EEG,ekgPeaks,eegChan);
-ampBad1=EEG.ampBad;
-ampStd1=EEG.ampBadStd;
-
-ampNeu1=EEG.ampNeu;
-ampNeuStd1=EEG.ampNeuStd;
-
-ampAlc1=EEG.ampAlc;
-ampAlcStd1=EEG.ampAlcStd;
-
-ampFod1=EEG.ampFod;
-ampFodStd1=EEG.ampFodStd;
-
-ampFix1=EEG.ampFix;
-ampFixStd1=EEG.ampFixStd;
-
-EEG = pop_biosig(ffName2);
-timeOutputs2 = tryEpochIndex(EEG,timeOutputs2);
-[EEG,params] = tryPreproc(EEG);
-EEG = tryEpocher(EEG,timeOutputs2);
-[ekgPeaks, params] = eeg_beats(EEG, params);
-eegChan = chanFinder(EEG,plotChan);
-EEG = lppParameters(EEG,timeOutputs1,eegChan);
-[bpm12, hr12, meanRR12, stdRR12, alphaPower12, alphaStd12,hrv12,hrvStd12] = calcBpm(EEG,ekgPeaks,eegChan);
-ampBad2=EEG.ampBad;
-ampStd2=EEG.ampBadStd;
-
-ampNeu2=EEG.ampNeu;
-ampNeuStd2=EEG.ampNeuStd;
-
-ampAlc2=EEG.ampAlc;
-ampAlcStd2=EEG.ampAlcStd;
-
-ampFod2=EEG.ampFod;
-ampFodStd2=EEG.ampFodStd;
-
-ampFix2=EEG.ampFix;
-ampFixStd2=EEG.ampFixStd;
-
-lppAmpNewNeuValues=[ampNeu1 ampNeu2];
-lppAmpNeuValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[ampNeuStd1 ampNeuStd2];
-lppAmpNeuStdValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[ampAlc1 ampAlc2];
-lppAmpAlcValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[ampAlcStd1 ampAlcStd2];
-lppAmpAlcStdValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[ampFod1 ampFod2];
-lppAmpFodValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[ampFodStd1 ampFodStd2];
-lppAmpFodStdValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[ampFix1 ampFix2];
-lppAmpFixValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[ampFixStd1 ampFixStd2];
-lppAmpFixStdValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmp=[ampBad1 ampBad2];
-lppAmpValues1(ki,:)=lppAmp;
-
-lppStdAmp=[ampStd1 ampStd2];
-lppAmpStdValues1(ki,:)=lppStdAmp;
-
-hr=[meanRR11 meanRR12];
-hrValues1(ki,:)=hr;
-
-bpm=[stdRR11 stdRR12];
-bpmValues1(ki,:)=bpm;
-
-ap=[alphaPower11 alphaPower12];
-apValues1(ki,:)=ap;
-
-apstd=[alphaStd1 alphaStd2];
-apstdValues1(ki,:)=apstd;
-
-hrv=[hrv1 hrv2];
-hrvValues(ki,:)=hrv;
-
-hrvstd=[hrvStd1 hrvStd2];
-hrvstdValues(ki,:)=hrvstd;
-
-hrv=[hrv11 hrv12];
-hrvValues1(ki,:)=hrv;
-
-hrvstd=[hrvStd11 hrvStd12];
-hrvstdValues1(ki,:)=hrvstd;
-
-neuCase=EEG.neuEpochs;
-alcCase=EEG.alcEpochs;
-fodCase=EEG.fodEpochs;
-fixCase=EEG.fixEpochs;
-
-lppNeuCells{mki}=neuCase;
-lppAlcCells{mki}=alcCase;
-lppFodCells{mki}=fodCase;
-lppFixCells{mki}=fixCase;
-
+hrvstdValues(ki,:)=hrvStd1;
+lppCue{mki}=EEG;
 mki=mki+1;
 
 catch
-lppAmp=[0 0];
+lppAmp=[0];
 lppAmpValues1(ki,:)=lppAmp;
 
-lppStdAmp=[0 0];
+lppStdAmp=[0];
 lppAmpStdValues1(ki,:)=lppStdAmp;
 
-hr=[0 0];
+hr=[0];
 hrValues(ki,:)=hr;
 
-bpm=[0 0];
+bpm=[0];
 bpmValues(ki,:)=bpm;
 
-ap=[0 0];
+ap=[0];
 apValues(ki,:)=ap;
 
-apstd=[0 0];
+apstd=[0];
 apstdValues(ki,:)=apstd;
 
 
-hr=[0 0];
+hr=[0];
 hrvValues(ki,:)=hr;
 
-hrvstd=[0 0];
+hrvstd=[0];
 hrvstdValues(ki,:)=hrvstd;
 
-
-bpm=[0 0];
-bpmValues1(ki,:)=bpm;
-
-ap=[0 0];
-apValues1(ki,:)=ap;
-
-apstd=[0 0];
-apstdValues1(ki,:)=apstd;
-
-
-hrv=[0 0];
-hrvValues1(ki,:)=hrv;
-
-hrvstd=[0 0];
-hrvstdValues1(ki,:)=hrvstd;
-
-
-
-hrv=[0 0];
-hrvValues1(ki,:)=hrv;
-
-hrvstd=[0 0];
-hrvstdValues1(ki,:)=hrvstd;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpNeuValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpNeuStdValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpAlcValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpAlcStdValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpFodValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpFodStdValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpFixValues(ki,:)=lppAmpNewNeuValues;
-
-lppAmpNewNeuValues=[0 0];
-lppAmpFixStdValues(ki,:)=lppAmpNewNeuValues;
 
 
 end
@@ -323,53 +198,20 @@ tryBatch={};
 restMeanTime=hrValues;
 restStdTime=bpmValues;
 
-cueMeanTime=hrValues1;
-cueStdTime=bpmValues1;
-
 restHrvMeanTime=hrvValues;
 restHrvStdTime=hrvstdValues;
-
-cueHrvTime=hrvValues1;
-cueHrvStdTime=hrvstdValues1;
 
 
 tryBatch{1}=restMeanTime;
 tryBatch{2}=restStdTime;
-tryBatch{3}=cueMeanTime;
-tryBatch{4}=cueStdTime;
+tryBatch{3}=restHrvMeanTime;
+tryBatch{4}=restHrvStdTime;
+tryBatch{5}=apValues;
+tryBatch{6}=apstdValues;
+tryBatch{7}=subNames;
 
-tryBatch{11}=apValues;
-tryBatch{12}=apstdValues;
-tryBatch{13}=apValues1;
-tryBatch{14}=apstdValues1;
-
-tryBatch{31}=restHrvMeanTime;
-tryBatch{32}=restHrvStdTime;
-tryBatch{33}=cueHrvTime;
-tryBatch{34}=cueHrvStdTime;
-
-tryBatch{40}=lppAmpNeuValues;
-tryBatch{41}=lppAmpNeuStdValues;
-
-tryBatch{42}=lppAmpAlcValues;
-tryBatch{43}=lppAmpAlcStdValues;
-
-tryBatch{44}=lppAmpFodValues;
-tryBatch{45}=lppAmpFodStdValues;
-
-tryBatch{46}=lppAmpFixValues;
-tryBatch{47}=lppAmpFixStdValues;
-
-tryBatch{51}=lppAmpValues1;
-tryBatch{52}=lppAmpStdValues1;
-
-
-save('tryCompareRestCue.mat',"tryBatch");
+save('tryRestEEG.mat',"tryBatch");
+save('tryLppCue.mat',"lppCue");
 
 %[h1,p1]=ttest2(bpmValues(:,1),bpmValues(:,2));
 %[h2,p2]=ttest2(hrValues(:,1),hrValues(:,2));
-
-save('lppNeuCells.mat','lppNeuCells');
-save('lppAlcCells.mat','lppAlcCells');
-save('lppFodCells.mat','lppFodCells');
-save('lppFixCells.mat','lppFixCells');
